8967db6067ba55047f54333facadadddbf210659
Faruk <farukumar41@gmail.com>
2024-04-20 14:52:40 +0100

    day 1

---
+++ build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+const myName = "Faruk";
+function addNumbers(a, b) {
+    return a + b;
+}
+console.log(addNumbers(5, 6));
---
+++ index.ts
@@ -0,0 +1,18 @@
+const firstName: string = "Faruk";
+const lastName: string = "Aminu";
+const collection: number[] = [1, 1, 1, 2, 1, 2, 4, 3, 4];
+
+function addNumbers(a: number, b: number) {
+    return a + b;
+}
+
+let sum = collection.reduce((accum, curr) => accum + curr, 0);
+const removeDuplicates = new Set(collection);
+console.log(Array.from(removeDuplicates))
+
+const makeUnique = (arr: number[]) => arr.filter((value, index, self) => self.indexOf(value) === index);
+// Example usage
+const array = [1, 2, 2, 3, 4, 4, 5];
+const uniqueArray = makeUnique(array);
+console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
+
